663fbb953a1b90983fa3641de24f76f1e70d387f
Tamas Marton <hello@tamasmarton.com>
2022-11-13 15:42:04 +0100

    Add new configs
    - Warp themes
    - Starship

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,31 @@
+# zsh aliases
+alias zshconfig="vim ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Composer aliases
+alias cup='composer update'
+alias cdu='composer dump'
+
+# Git aliases
+alias ga='git add'
+alias gs='git status'
+alias gc='git commit'
+alias gpu='git pull'
+alias gpuo='git pull origin'
+alias gp='git push'
+alias gpo='git push origin'
+alias gcu='git checkout'
+alias gcb='git checkout -b'
+alias gd='git diff'
+
+# Current weather
+alias wttr='curl -s http://wttr.in/Budapest | head -7 | tail -5'
+
+# GOTO Sites folder
+alias sites='cd ~/Sites/'
+
+# ALIAS COMMANDS
+# alias ls="exa --icons --group-directories-first"
+# alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
